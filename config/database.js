0d250ac52acd68ece600a98cc90cf9b22b4342d2
Ricardo Ribeiro <ricardo.erik@gmail.com>
2022-09-19 17:59:09 -0300

    database configs

--- config/database.js
+++ config/database.js
@@ -2,12 +2,15 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST"),
-      port: env.int("DATABASE_PORT"),
-      database: env("DATABASE_NAME"),
-      user: env("DATABASE_USERNAME"),
-      password: env("DATABASE_PASSWORD"),
-      ssl: env.bool("DATABASE_SSL"),
+      host: env(
+        "DATABASE_HOST",
+        "mepcms-databases.c8zj6owqn1oo.us-east-1.rds.amazonaws.com"
+      ),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "mepcmsstrapi"),
+      user: env("DATABASE_USERNAME", "mepcms"),
+      password: env("DATABASE_PASSWORD", "w7EfQ2WEQal_dMNtXJ7whWssbDuUMH2_"),
+      ssl: env.bool("DATABASE_SSL", false),
     },
   },
 });
